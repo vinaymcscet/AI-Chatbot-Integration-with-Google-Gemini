e57448608283e5af91503f4b38b86f5e8612d35b
Vinay kumar <vinay.mcscet68@gmail.com>
2026-02-18 18:32:59 +0530

    update backend live url

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import { GoogleGenAI } from "@google/genai";
 dotenv.config();
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "*",
   methods: ["GET", "POST"],
   credentials: true
 }));
